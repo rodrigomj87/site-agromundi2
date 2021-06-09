c8963df1bc4b7467ddbf77d182ec4f2b6b7e2375
RODRIGO MARTINS <rodrigomj@icloud.com>
2021-06-09 16:50:18 -0300

    melhorias

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,29 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// File#: _2_modal-video
+$(document).ready(function () {
+    // Gets the video src from the data-src on each button
+
+    var $videoSrc;
+    $('.video-btn').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+
+    // when the modal is opened autoplay it  
+    $('#myModal').on('shown.bs.modal', function (e) {
+
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+
+    // stop playing the youtube video when I close the modal
+    $('#myModal').on('hide.bs.modal', function (e) {
+        // a poor man's stop video
+        $("#video").attr('src', $videoSrc);
+    })
+    // document ready  
+});
+
