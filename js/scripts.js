8d8b079a70dd954bb97aefac0e9875898a68590b
RODRIGO MARTINS <rodrigomj@icloud.com>
2021-06-17 04:54:10 -0300

    ajustes ++

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,8 @@ $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
     container: 'body'
 });
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
