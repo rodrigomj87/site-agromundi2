ebdb43b9f1157f7702b855f92a360bcecdb0a03b
RODRIGO MARTINS <rodrigomj@icloud.com>
2021-06-19 13:30:58 -0300

    ajustes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,3 +40,10 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
 })
+
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function() {
+//     myInput.focus()
+// })
